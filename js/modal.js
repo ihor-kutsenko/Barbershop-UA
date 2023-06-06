39a1147aeed9c93f77e66fe2ba13d68d712061fe
Rammbest <igor-kutsenko@ukr.net>
2023-06-06 12:27:56 +0300

    add a modal window to each button

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,15 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    otherButtons: document.querySelectorAll('.open-modal'), // замініть на ваш селектор для інших кнопок
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.otherButtons.forEach(button => {
+    button.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
